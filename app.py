7380bbd665a216371fe7d262d261fac41810499d
sabari-sk <sabarikarthikeyan042.it@dgct.ac.in>
2025-09-21 17:05:15 +0530

    Add gunicorn to requirements.txt for Render deployment

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def send_file(filename):
 	return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=False)
