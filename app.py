f8842ca3c46f655d23c3af06ea98a1a6725d0a52
Sabari KarthiKeyan R <sabarikarthikeyan042.it@dgct.ac.in>
2025-09-21 17:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,4 @@ def send_file(filename):
 	return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=False)
+	app.run(debug=True)
